6fe5029c31f6ef1576b06f52240cdc3727f19bfd
Monireh Jamshidi <monireh.jamshidi@gmail.com>
2023-12-31 22:37:40 +0330

    we rendered products

--- src/pages/ProductsPages.jsx
+++ src/pages/ProductsPages.jsx
@@ -1,10 +1,19 @@
 import { useProducts } from "../context/ProductContext";
+import styles from "./ProductsPages.module.css";
 
 function ProductsPages() {
   const products = useProducts();
   console.log(products);
   return (
-    <div>ProductsPages</div>
+    <div className={styles.container}>
+      <div className={styles.products}>
+        {!products.length && <p>Loading...</p>}
+        {products.map(p => (
+          <p key={p.id}>{p.title}</p>
+        ))}
+      </div>
+      <div>SideBar</div>
+    </div>
   )
 }
 
