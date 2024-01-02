86e79484d4daa37259935450612ec7da592df8b9
Monireh Jamshidi <monireh.jamshidi@gmail.com>
2024-01-02 23:37:07 +0330

    create search bar part

--- src/pages/ProductsPages.jsx
+++ src/pages/ProductsPages.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { ImSearch } from "react-icons/im";
+
 import Card from "../components/Card";
 import Loader from "../components/Loader";
 import { useProducts } from "../context/ProductContext";
@@ -6,16 +9,27 @@ import styles from "./ProductsPages.module.css";
 function ProductsPages() {
   const products = useProducts();
   
+  const [search , setSearch] = useState("");
+
+  const searchHandler = () => {
+    console.log("search")
+  }
   return (
-    <div className={styles.container}>
-      <div className={styles.products}>
-        {!products.length && <Loader />}
-        {products.map(product => (
-          <Card key={product.id} data={product} />
-        ))}
+    <>
+      <div>
+        <input type="text" placeholder="Search..." value={search} onChange={e => setSearch(e.target.value.toLocaleLowerCase().trim())} />
+        <button onClick={searchHandler}><ImSearch /></button>
       </div>
-      <div>SideBar</div>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.products}>
+          {!products.length && <Loader />}
+          {products.map(product => (
+            <Card key={product.id} data={product} />
+          ))}
+        </div>
+        <div>SideBar</div>
+      </div>
+    </>
   )
 }
 
