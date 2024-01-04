41869832dc9d9f4f22776c1c5d8e96cb14f96eb0
Monireh Jamshidi <monireh.jamshidi@gmail.com>
2024-01-04 23:07:25 +0330

    get query and do it on loading the page

--- src/pages/ProductsPages.jsx
+++ src/pages/ProductsPages.jsx
@@ -7,7 +7,7 @@ import Card from "../components/Card";
 import Loader from "../components/Loader";
 import { useProducts } from "../context/ProductContext";
 import styles from "./ProductsPages.module.css";
-import { createQueryObject, filterProducts, searchProducts } from "../helper/helper";
+import { createQueryObject, filterProducts, getInitialQuery, searchProducts } from "../helper/helper";
 
 
 function ProductsPages() {
@@ -21,11 +21,14 @@ function ProductsPages() {
 
   useEffect(() => {
     setDisplayed(products);
+
+
+    setQuery(getInitialQuery(searchParams));
   },[products]);
 
   useEffect(() => {
     setSearchParams(query);
-
+    setSearch(query.search || "");
     let finalProducts = searchProducts(products, query.search);
     finalProducts = filterProducts(finalProducts, query.category);
 
