85542978d9f132d4ac8b90afa224681475cc9482
Monireh Jamshidi <monireh.jamshidi@gmail.com>
2024-01-08 20:57:20 +0330

    create context add it to my project and create a custom Hook to give it state and dispatch that context gave us

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,9 +4,16 @@ import {TbShoppingBagCheck} from "react-icons/tb";
 import { shortenText } from "../helper/helper";
 
 import styles from "./Card.module.css";
+import { useCart } from "../context/CartContext";
 
 function Card({data}) {
     const {id, title, image, price} = data;
+    const [state, dispatch] = useCart();
+
+    const clickHandler = () => {
+      dispatch({type: "add", payload: data})
+    }
+
   return (
     <div className={styles.card}>
         <img src={image} alt={title} />
@@ -15,7 +22,9 @@ function Card({data}) {
         <div className={styles.actions}>
             <Link to={`/products/${id}`}><TbListDetails /></Link>
             <div>
-                <button><TbShoppingBagCheck /></button>
+                <button onClick={clickHandler}>
+                  <TbShoppingBagCheck />
+                </button>
             </div>
         </div>
     </div>
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,17 +1,25 @@
-import { createContext, useReducer } from "react"
+import { createContext, useContext, useReducer } from "react"
 
 const initialState = {};
 
-const reducer = () => {}
+const reducer = (stae, action) => {
+  console.log(action)
+}
 const CartContext = createContext();
 
 function CartProvider({children}) {
     const [state, dispatch] = useReducer(reducer, initialState);
   return (
-    <CartContext.Provider value={state}>
+    <CartContext.Provider value={{state, dispatch}}>
         {children}
     </CartContext.Provider>
   )
 }
 
+const useCart = () => {
+  const { state, dispatch } = useContext(CartContext);
+  return[state, dispatch]
+}
+
 export default CartProvider
+export {useCart};
