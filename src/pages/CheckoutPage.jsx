e8f3e6dec36193dc82e280932f25ae528e496c9c
Monireh Jamshidi <monireh.jamshidi@gmail.com>
2024-01-13 23:28:01 +0330

    implement buy basket page

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,6 +1,19 @@
+import BasketCard from "../components/BasketCard";
+import { useCart } from "../context/CartContext"
+
 function CheckoutPage() {
+  const [state, dispatch] = useCart();
+  console.log(state)
+
+  const clickHandler = (type, payload) => dispatch({type, payload});
   return (
-    <div>CheckoutPage</div>
+    <div>
+      <div>
+        {state.selectedItems.map((product) => (
+          <BasketCard key={product.id} data={product} clickHandler={clickHandler} />
+        ))}
+      </div>
+    </div>
   )
 }
 
