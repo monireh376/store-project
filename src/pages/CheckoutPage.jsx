3c58e9747a1c1f3e0c50537ae09489d60556832d
Monireh Jamshidi <monireh.jamshidi@gmail.com>
2024-01-15 21:44:21 +0330

    finish sideBar related to store page

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,14 +1,22 @@
 import BasketCard from "../components/BasketCard";
+import BasketSidebar from "../components/BasketSideBar";
 import { useCart } from "../context/CartContext"
+
+import styles from "./CheckoutPage.module.css";
 
 function CheckoutPage() {
   const [state, dispatch] = useCart();
   console.log(state)
 
   const clickHandler = (type, payload) => dispatch({type, payload});
+
+  if (!state.itemsCounter) {
+    return (<div className={styles.container}>Empty</div>)
+  }
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <BasketSidebar state={state} clickHandler={clickHandler} />
+      <div className={styles.products}>
         {state.selectedItems.map((product) => (
           <BasketCard key={product.id} data={product} clickHandler={clickHandler} />
         ))}
